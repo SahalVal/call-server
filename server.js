473338d89a11e6fcceace989597352b32a977baa
SahalVal <valentinsahali@gmail.com>
2025-06-19 13:06:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const WebSocket = require('ws');
 const express = require('express');
 const http = require('http');
@@ -9,29 +8,13 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', ws => {
   console.log('Twilio connecté');
-
-  ws.on('message', async data => {
-    const msg = JSON.parse(data);
-
-    if (msg.event === 'media') {
-      const audioData = msg.media.payload;
-      // TODO : envoyer ce son vers GPT-4o realtime
-    }
-
-    if (msg.event === 'start') {
-      console.log('Appel démarré');
-    }
-
-    if (msg.event === 'stop') {
-      console.log('Appel terminé');
-    }
+  ws.on('message', data => {
+    console.log('Reçu :', data);
+    // Ici tu pourras ajouter GPT-4o plus tard
   });
-
-  ws.on('close', () => {
-    console.log('Connexion fermée');
-  });
+  ws.on('close', () => console.log('Connexion fermée'));
 });
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log('Serveur WebSocket démarré');
+  console.log('Serveur WebSocket en ligne sur le port 3000');
 });
