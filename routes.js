22d704c591c4d2bd9ddfdea60ff0a1150edf2185
SahalVal <valentinsahali@gmail.com>
2025-06-20 14:09:50 +0200

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,20 @@
+export async function initRoutes(fastify) {
+  fastify.route({
+    method: 'POST',
+    url: '/call',
+    handler: handleCall,
+  });
+
+  fastify.route({
+    method: 'POST',
+    url: '/webhook/recording',
+    handler: handleRecordingWebhook,
+  });
+
+  fastify.route({
+    method: 'GET',
+    url: '/media-stream',
+    websocket: true,
+    handler: setupWebSocket,
+  });
+}
