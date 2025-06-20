e2aa5ae2819d6d4af98b22ed9b202964d2289db6
SahalVal <valentinsahali@gmail.com>
2025-06-20 14:00:02 +0200

    Update route.js

--- route.js
+++ route.js
@@ -1,8 +1,20 @@
-import { handleCall, handleRecordingWebhook } from './twilio.js';
-import { setupWebSocket } from './websocket.js';
-
 export async function initRoutes(fastify) {
-  fastify.post('/call', handleCall);
-  fastify.post('/webhook/recording', handleRecordingWebhook);
-  fastify.get('/media-stream', { websocket: true }, setupWebSocket);
+  fastify.route({
+    method: 'POST',
+    url: '/call',
+    handler: handleCall,
+  });
+
+  fastify.route({
+    method: 'POST',
+    url: '/webhook/recording',
+    handler: handleRecordingWebhook,
+  });
+
+  fastify.route({
+    method: 'GET',
+    url: '/media-stream',
+    websocket: true,
+    handler: setupWebSocket,
+  });
 }
